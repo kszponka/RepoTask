a95d106fd8422672e659b3ef64c041d3068867e3
skrzychuz@wp.pl <skrzychuz@wp.pl>
2018-04-07 09:56:21 +0200

    initial commit

---
+++ src/main/java/software/engineer/szponka/SzponkaApplication.java
@@ -0,0 +1,12 @@
+package software.engineer.szponka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzponkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzponkaApplication.class, args);
+	}
+}
